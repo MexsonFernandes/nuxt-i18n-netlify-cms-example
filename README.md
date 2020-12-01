fa3235303ffaa839ff3f4ac512340759fad9bb52
Mexson Fernandes <robomex2020@gmail.com>
2020-12-02 00:39:06 +0530

    :sparkles: i18n version with json files

---
+++ README.md
@@ -0,0 +1,24 @@
+# nuxt-i18n-netlify-cms-example
+
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FMexsonFernandes%2Fnuxt-i18n-netlify-cms-example&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+## References
+
+* [Nuxt i18n external JSON setup](https://phrase.com/blog/posts/nuxt-js-tutorial-i18n/#External_JSON_or_JS_Files)
---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+import i18n from './config/i18n'
+
+export default {
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'nuxt-i18n-netlify-cms-example',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/tailwindcss',
+    [
+      'nuxt-i18n',
+      {
+        defaultLocale: 'en',
+        vueI18nLoader: true,
+        locales: [
+          {
+            code: 'en',
+            name: 'English',
+          },
+          {
+            code: 'fr',
+            name: 'Français',
+          },
+        ],
+        vueI18n: i18n,
+      },
+    ],
+  ],
+}
